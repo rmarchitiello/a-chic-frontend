387cd7bb3faefaf0eef0f076a8c20dd0356bf3f1
robertomarchitiello <roberto.marchitiello@lutech.it>
2025-06-21 10:52:40 +0200

    prova

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,21 +1,32 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (config) => {
-  config.optimization = {
-    ...config.optimization,
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            global_defs: {
-              ngDevMode: false  
+module.exports = (config, options) => {
+  // fallback nel caso configuration sia undefined
+  const isProd = process.env.NODE_ENV === 'production' || config.mode === 'production';
+
+  console.log(">> CUSTOM WEBPACK CONFIG ACTIVE <<", {
+    mode: config.mode,
+    nodeEnv: process.env.NODE_ENV,
+    optionsConfiguration: options.configuration
+  });
+
+  if (isProd) {
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+              global_defs: {
+                ngDevMode: false
+              }
             }
           }
-        }
-      })
-    ]
-  };
+        })
+      ]
+    };
+  }
+
   return config;
 };
