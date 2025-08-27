3c1809199dfa4e9001df5d148f244c32c580582e
robertomarchitiello <roberto.marchitiello@lutech.it>
2025-08-27 17:49:37 +0200

    a

--- src/app/pages/top-banner/top-banner.component.ts
+++ src/app/pages/top-banner/top-banner.component.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { SharedDataService } from '../../services/shared-data.service';
 import { Subject, takeUntil } from 'rxjs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 import { TextConfigFromCache, PageConfig, ComponentBlock, DynamicItem } from '../../app.component';
+import { FormsModule } from '@angular/forms';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 @Component({
   selector: 'app-top-banner',
   standalone: true,               // componente standalone (senza modulo dedicato)
-  imports: [CommonModule],        // importa CommonModule per *ngIf, *ngFor ecc.
+  imports: [CommonModule,MatTooltipModule,MatIconModule,FormsModule],        // importa CommonModule per *ngIf, *ngFor ecc.
   templateUrl: './top-banner.component.html',
   styleUrls: ['./top-banner.component.scss']
 })
@@ -15,10 +20,23 @@ export class TopBannerComponent implements OnInit {
   // Subject usato come notifica di distruzione per cancellare le subscription
   private readonly destroy$ = new Subject<void>();
 
+imEditing = false;
+@ViewChild('bannerInput') bannerInput?: ElementRef<HTMLInputElement>;
 
+abilitaModifica(): void {
+  this.imEditing = !this.imEditing;
+  setTimeout(() => {
+    const el = this.bannerInput?.nativeElement;
+    if (el) {
+      el.focus();
+      el.setSelectionRange(el.value.length, el.value.length); // cursore a fine testo (facoltativo)
+    }
+  });
+  console.log("Banner text: ", this.bannerText);
+}
   // Flag che indica se l’utente è su mobile (<= 768px)
   isMobile = false;
-
+  isAdmin: boolean = false;
   // Testo da mostrare nel banner (duplicato in HTML per lo scroll continuo)
   bannerText: string =
     '🚧 Sito in aggiornamento // Tutti gli articoli sono sold out // Seguici sui social per restare aggiornati sulle prossime novità ✨';
@@ -42,6 +60,14 @@ export class TopBannerComponent implements OnInit {
           this.bannerText = '';
         },
       });
+
+          this.sharedDataService.isAdmin$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(isAdmin => {
+        this.isAdmin = isAdmin;
+        console.log("Banner in admin?: ", this.isAdmin);
+      });
+
 
     // Osserva i cambi di dimensione dello schermo
     // Se la viewport è <= 768px imposta isMobile = true
